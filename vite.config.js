1e395545b72fd54a4820222b2e548bc480c009c9
skrillcii <skrillcii@github.com>
2024-05-28 06:37:42 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react_three_app_demo/'
   plugins: [react()],
   assetsInclude: ["**/*.glb"],
 });
